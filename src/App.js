870d2bdbe3aeae49214532cd744d63b9a102d8fd
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-12-20 02:42:22 +0300

    add play and pause logic

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import data from './data';
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
     </div>
   );
 }
