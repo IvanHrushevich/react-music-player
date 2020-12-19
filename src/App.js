40e9cab4986caae6508ea8f868ea7098c0922cc8
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-12-20 01:36:24 +0300

    implement Player and Song components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.scss';
+import './styles/app.scss';
+
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
